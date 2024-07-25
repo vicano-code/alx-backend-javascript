837a3caf1d8c6bccffc3dcae753f31bf8e677a19
vicano-code <vc.anokwuru@gmail.com>
2024-07-25 22:03:58 +0100

    review code

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -3,17 +3,27 @@ export default class Car {
     if (typeof brand !== 'string') throw TypeError('brand must be a string');
     if (typeof motor !== 'string') throw TypeError('motor must be a string');
     if (typeof color !== 'string') throw TypeError('color must be a string');
+
     this._brand = brand;
     this._motor = motor;
     this._color = color;
   }
 
-  static get [Symbol.species]() {
-    return this;
+  // Getters for the attributes
+  get brand() {
+    return this._brand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  get color() {
+    return this._color;
   }
 
   cloneCar() {
-    const Species = this.constructor[Symbol.species];
-    return new Species(this._brand, this._motor, this._color);
+    // Use the constructor of the current instance to create a new one
+    return new this.constructor(this._brand, this._motor, this._color);
   }
 }
