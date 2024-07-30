fc42f24fdcb09ee0245240b170ceb471a12dece8
vicano-code <vc.anokwuru@gmail.com>
2024-07-30 11:57:30 +0100

    review task code to correct for 'species' requirement

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -9,21 +9,12 @@ export default class Car {
     this._color = color;
   }
 
-  // Getters for the attributes
-  get brand() {
-    return this._brand;
-  }
-
-  get motor() {
-    return this._motor;
-  }
-
-  get color() {
-    return this._color;
+  static get [Symbol.species]() {
+    return this;
   }
 
   cloneCar() {
-    // Use the constructor of the current instance to create a new one
-    return new this.constructor(this._brand, this._motor, this._color);
+    const Species = this.constructor[Symbol.species];
+    return new Species(this._brand, this._motor, this._color);
   }
 }
