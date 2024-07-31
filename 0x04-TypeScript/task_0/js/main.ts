b71197ad6a8488a1889d0b8f2be1c65466af0d57
vicano-code <vc.anokwuru@gmail.com>
2024-07-31 22:53:39 +0100

    review code to display table on localhost

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -4,28 +4,47 @@ interface Student {
   age: number;
   location: string;
 };
-const student1: Student {
-  firstName: 'David';
-  lastName: 'Heavens';
-  age: 23;
-  location: 'New York';
+const student1: Student = {
+  firstName: 'David',
+  lastName: 'Heavens',
+  age: 23,
+  location: 'New York',
 };
-const student2: Student {
-  firstName: 'Gabby';
-  lastName: 'Blessed';
-  age: 25;
-  location: 'Florida';
+const student2: Student = {
+  firstName: 'Gabby',
+  lastName: 'Blessed',
+  age: 25,
+  location: 'Florida',
 };
-const studentsList: Student[] = [student1, student2];
+const studentsList: Array<Student> = [student1, student2];
 
-const myTable = document.createElement('table');
-studentsList.forEach(student => {
-  let row = document.createElement('tr');
-  let col1 = document.createElement('td');
-  let col2 = document.createElement('td');
-  col1.textContent = student.firstName;
-  col2.textContent = student.location;
-  row.appendChild(col1);
-  row.appendChild(col2);
-  myTable.appendChild(row);
-})
+const body: HTMLBodyElement = document.getElementsByTagName("body")[0];
+const table: HTMLTableElement = document.createElement('table');
+const thead: HTMLTableSectionElement = document.createElement('thead');
+const th1: HTMLTableCellElement = document.createElement('th');
+const th2: HTMLTableCellElement = document.createElement('th');
+
+table.style.border = '1px solid gray';
+table.style.borderCollapse = 'collapse';
+
+th1.innerText = 'First Name';
+th2.innerText = 'Location';
+thead.append(th1);
+thead.append(th2);
+table.append(thead);
+
+studentsList.forEach((student) => {
+  let row: HTMLTableRowElement = document.createElement('tr');
+  let col1: HTMLTableCellElement = document.createElement('td');
+  let col2: HTMLTableCellElement = document.createElement('td');
+  col1.innerText = student.firstName;
+  col2.innerText = student.location;
+  row.append(col1);
+  row.append(col2);
+  table.append(row);
+
+  col1.style.border = '1px solid gray';
+  col2.style.border = '1px solid gray';
+});
+
+body.append(table)
