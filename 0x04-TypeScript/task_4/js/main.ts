6b13b9584c46a3d0ea3b0929dbb448659700d20a
vicano-code <vc.anokwuru@gmail.com>
2024-07-31 22:24:10 +0100

    new tasks & code review of previous tasks

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -1,9 +1,9 @@
-/// <reference path="./Cpp.ts" />
-/// <reference path="./Java.ts" />
-/// <reference path="./React.ts" />
+/// <reference path="./subjects/Cpp.ts" />
+/// <reference path="./subjects/Java.ts" />
+/// <reference path="./subjects/React.ts" />
 
-import { Subjects } from './Subject';
-import { Teacher } from './Teacher';
+
+
 
 export const cpp = new Subjects.Cpp();
 export const java = new Subjects.Java();
