937aac0af842175a19fab9412f7cd0cbeb8fd1d7
Victor Anokwuru <vc.anokwuru@gmail.com>
2024-09-19 00:59:15 +0100

    review previous change

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -5,7 +5,6 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.send('Hello Holberton School!');
 });
 
