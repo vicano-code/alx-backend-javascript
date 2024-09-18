92b69d5d013b619cbb55eb4c1ca9c05ff94857b0
Victor Anokwuru <vc.anokwuru@gmail.com>
2024-09-19 00:53:02 +0100

    make module return plane text/add return

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -5,6 +5,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.send('Hello Holberton School!');
 });
 
